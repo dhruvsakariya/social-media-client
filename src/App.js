e03469cfcbaa44a2037ab16b27bcbd5c4a7e7f69
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-20 10:57:54 +0530

    add custom nav bar and fix semantic-ui bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
-function App() {
-  return <div>hello dhruv</div>;
-}
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default App;
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+// import MenuBar from "./components/MenuBar";
+// import 'semantic-ui-css/semantic.min.css'
+import "./App.css";
+export default function App() {
+  return (
+    <Router>
+      {/* <MenuBar /> */}
+      <div>
+        <nav>
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link className="link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="link" to="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import ApolloProvider from "./ApolloProvider";
 import reportWebVitals from "./reportWebVitals";
-
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
