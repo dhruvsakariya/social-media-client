9c54a652ab5d36dbab94d82f03eb3ee1ab87da9c
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-26 10:35:19 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  uri: "https://gentle-beyond-16416.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
